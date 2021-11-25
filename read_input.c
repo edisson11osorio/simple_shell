648b4d857b2da837e83a6c0aa991bcd57ff0eda9
joacasallas <joacasallas@gmail.com>
2021-11-25 15:31:37 -0500

    update

---
+++ read_input.c
@@ -0,0 +1,6 @@
+#include <shell.h>
+
+ssize_t read_input(char *input)
+{
+	input = ssize_t getline(input, strlen(input), 0);
+}
--- shell.h
+++ shell.h
@@ -19,7 +19,7 @@
 #include <fcntl.h>
 #include <stddef.h>
 
-#define PROMPT ("$  ")
+#define PROMPT ("$ ")
 
 void prompt(void); 
 
