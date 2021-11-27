55399fa4a1c94122ca7663a8460b4f749b0c7d42
joacasallas <joacasallas@gmail.com>
2021-11-27 15:11:14 -0500

    update2

--- read_input.c
+++ read_input.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
-int read_input(char *data)
+char read_input(char *data)
 {
-	int read_input;
-	char *buffer;
-	ssize sizebuffer = strlen(*data)
+	ssize_t read_input;
+	char **buffer;
+	size_t sizebuffer = strlen(*data);
 
 	read_input = getline(buffer, sizebuffer, 1);
 	if (read_input == NULL)
