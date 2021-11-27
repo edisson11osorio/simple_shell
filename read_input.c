c954dfa0d07bf859bf5d28d12cc6baefe8db7840
edisson <osorioedisson08@gmail.com>
2021-11-27 15:26:29 -0500

    changes_and_errors

--- read_input.c
+++ read_input.c
@@ -3,8 +3,8 @@
 int read_input(char *data)
 {
 	int read_input;
-	char *buffer;
-	ssize sizebuffer = strlen(*data)
+	char **buffer;
+	ssize_t sizebuffer = strlen(data);
 
 	read_input = getline(buffer, sizebuffer, 1);
 	if (read_input == NULL)
--- tokenizer.c
+++ tokenizer.c
@@ -2,19 +2,18 @@
 
 char tokenizer(char *read_input)
 {
-	char *tokens[];
 	int i;
+	char **tokens = malloc(sizeof(char *) * 1024);
 
-	tokens = malloc(sizeof(char *)* 1024);
 	if (tokens == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	i = 0;
 	while (read_input != NULL)
 	{
-		tokens[i] = strtok(read_input," ");
+		tokens[i] = strtok(read_input, " ");
 		i++;
 	}
-	return (tokens);
+	return (0);
 }
