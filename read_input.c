3ce56664cefc2b610d797a968659cb28b1e02a5c
joacasallas <joacasallas@gmail.com>
2021-11-28 10:43:15 -0500

    update read_input

--- read_input.c
+++ read_input.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
-ssize_t *read_input(char **data, size_t *sizebuffer)
+char *read_input(char **data, size_t *sizebuffer)
 {
 	ssize_t read_input = 0;
 
 	read_input = getline(data, sizebuffer, stdin);
-	if (read_input == NULL)
+	if (read_input == '\0')
 	{
-		free(*data)
+		free(*data);
 		return (NULL);
 	}
 	return (*data);
