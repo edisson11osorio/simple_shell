f8e05cafb0a21d5d21bf3664a7e1786b4104e7ed
joacasallas <joacasallas@gmail.com>
2021-11-30 05:12:19 -0500

    update

---
+++ exec_Command.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+char *execute(char *command, char *builtinlist)
+{
+	char *solution;
+	
+	if (strcmp(command, builtintlist[0] == 0))
+	{
+		solution = builtinlist[0];
+	}
+	else
+	{
+		solution = builtinlist[1];
+	}
+	return (solution)
+}
--- shell.c
+++ shell.c
@@ -8,15 +8,18 @@ int main()
 {
 	char *data = NULL;
 	size_t sizebuffer = 0;
+	char *command = data;
 	char *tokens;
 
 	while (1)
 	{
 		prompt();
 		data = read_input(&data, &sizebuffer);
-		tokens = tokenizer(data);
+		tokens = tokenizer(command);
 		printf("print data content [%s]\n", data);
+		printf("print command content[%s]\n", command);
 		printf("print tokens content [%s]\n", tokens);
+		exec_Command = execute(command, builtinlist);
 	}
 	return 0;
 }
