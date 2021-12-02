177723fe36194ebe6fd19c7caf1fe4474cf0175b
edisson <osorioedisson08@gmail.com>
2021-12-02 12:03:26 -0500

    stat.c

---
+++ stat.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+int main()
+{
+    for (i = 0; *directories[i] != '\0'; i++)
+    {
+        if (function == 0)
+        {
+            return (0);
+        }
+        function = stat(directories[i], token);
+    }
+}
