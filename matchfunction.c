4b107480e4c2d7eae63254fa47a740959af28efc
joacasallas <joacasallas@gmail.com>
2021-12-02 17:06:04 -0500

    update

--- matchfunction.c
+++ matchfunction.c
@@ -2,17 +2,19 @@
 
 char *matchfunction(char **token, char **directories)
 {
-	char *execfile = malloc(sizeof(char) * 1024);
 	int i, function = 1;
 	struct stat *buf = malloc(1024);
-	char *concat_1, *concat_2;
+	char *concat_2 = malloc(1024);
+	char *concat_1 = malloc(1024);
 
 	concat_1 = strcat("/", token[0]);
+	printf("%s", concat_1);
+	printf("%s", directories[0]);
 	for (i = 0; *directories[i] != '\0' || function != 0; i++)
 	{
 		concat_2 = strcat(directories[i], concat_1);
+		printf("%s", concat_2);
 		function = stat(concat_2, buf);
-	}
-	execfile = concat_2;
-	return (execfile);
+		}
+	return (concat_1);
 }
